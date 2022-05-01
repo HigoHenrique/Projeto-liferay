964b74258c31f9cf4ce8dc2fd862fa0dbe37e53a
Camilla <camilla.taina.santos@accenture.com>
2022-05-01 13:59:54 -0300

    criação dos components menu e formulario

--- web-liferay/src/app/app-routing.module.ts
+++ web-liferay/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './component/formulario/formulario.component';
+import { MenuComponent } from './component/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ' ', component: AppComponent},
+  { path: 'formulario', component: FormularioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-liferay/src/app/app.module.ts
+++ web-liferay/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { FormularioComponent } from './component/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FormularioComponent,
   ],
   imports: [
     BrowserModule,
